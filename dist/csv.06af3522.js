0c99c08dda66ddcfb04dece4656004d923cb5246
nzambello <nzambello@protonmail.com>
2022-12-06 16:27:01 +0100

    chore: ignore dist in git

--- dist/csv.06af3522.js
+++
@@ -1,1 +0,0 @@
-("undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{}).parcelRequire039d.register("gQtee",(function(e,n){"use strict";function i(e){e.languages.csv={value:/[^\r\n,"]+|"(?:[^"]|"")*"(?!")/,punctuation:/,/}}e.exports=i,i.displayName="csv",i.aliases=[]}));
